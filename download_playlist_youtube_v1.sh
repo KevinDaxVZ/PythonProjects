4e6442e2c752d2a6754e43f1a46a51ddedc85ab3
Kevin Dax Victorio Zanabria <40190622+KevinDaxVZ@users.noreply.github.com>
2024-08-12 00:56:56 -0500

    Update and rename download_playlist_youtube.sh to download_playlist_youtube_v1.sh

--- download_playlist_youtube_v1.sh
+++ download_playlist_youtube_v1.sh
@@ -1,3 +1,6 @@
+#TENGO UNA VERSION 2 DE ESTE SCRIPT, EL CUAL SI DESCARGA EN LA CALIDAD MAS ALTA LOS VIDEOS DE YOUTUBE, ESTA SOLO LOS DESCARGA POR LO GENERAL EN UNA CALIDAD ESTANDAR
+#REVISA MI VERSION 2
+
 sudo apt install python3 -y 
 pip install yt-dlp
 sudo apt install ffmpeg -y
@@ -10,3 +13,5 @@ echo -e '\t*****¡ATENCION!****'
 echo -ne "Los videos se descargar en el directorio actual en el que estas$(pwd)\n"
 read -p 'Ingrese la URL de tu playlist: '
 yt-dlp -f 'bestvideo[height<=?1080]+bestaudio/best[height<=?1080]' "$REPLY"
+
+
