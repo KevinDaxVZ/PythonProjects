c8395abca719068fa86664a328b04d1e0c190724
Kevin Dax Victorio Zanabria <40190622+KevinDaxVZ@users.noreply.github.com>
2024-10-30 14:09:36 -0500

    Update crack_elf.py

--- crack_elf.py
+++ crack_elf.py
@@ -7,7 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 def main():
     """f_leer = ['super_secret_password'] <-- POSSIBLE PASSWORD"""
-    archivo = sys.argv[1]
+    archivo = sys.argv[2]
 
     if sys.argv[1].lower() == '-h' or sys.argv[1] == '--hh':
         help()
@@ -16,7 +16,6 @@ def main():
     with open(archivo,'r',encoding='latin-1') as f:
         f_leer = [linea.strip() for linea in f.readlines()]
 
-    
 
     with ThreadPoolExecutor(max_workers=5) as executor:
         executor.map(attack,f_leer)
@@ -24,9 +23,11 @@ def main():
 
 def attack(password):
     
-        result = subprocess.run(['./crackme2',password],capture_output=True,text=True)
+        result = subprocess.run([sys.argv[1],password],capture_output=True,text=True)
 
-        if "Access denied" not in result.stdout:
+        message = sys.argv[3]
+
+        if message not in result.stdout:
             print(result.stdout.strip(),end=' ')
             print(f'{colorama.Fore.GREEN}\tPASSWORD: {password}{colorama.Fore.RESET}')
             
@@ -38,14 +39,16 @@ def attack(password):
         else:
             print(f'{colorama.Fore.RED}{result.stdout.strip()}',end=' ')
             print(f'\tfor PASSWORD: {password}{colorama.Fore.RESET}')
-
+            
 
 def help():
     print('Correct example use: ')
-    print('\tcracking_ELF.py NAMEFICHERO')
+    print('\tcracking_ELF.py NAMEPROGRAM WORDLIST MESSAGE\n')
+    print('\tcracking_ELF.py ./cracking4 /usr/share/wordlist/rockyou.txt "Acces Denied"')
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 4:
             main()
 
     else:
